79498102a60d0f41aa56af3fc53df8b2b1dcd47a
Quy, Nguyen Huu (Mr.) <nguyenquy.1096@gmail.com>
2022-11-13 14:17:33 +0700

    no message

--- src/routes/index.js
+++ src/routes/index.js
@@ -34,4 +34,12 @@ router.get('/zalo_verifierGjIaShB99sHZk85uWuKrJrxmu1YoXJ9ED3K.html', (req, res, 
         });
 });
 
+router.get('/zalo_verifierOV2m2f7LF1TmmkWDekC-4cYKbmQ4uaTQD3S.html', (req, res, next) => {
+    return res
+        .status(200)
+        .render('zalo_verifierOV2m2f7LF1TmmkWDekC-4cYKbmQ4uaTQD3S', {
+            title: "Nguyen Quy Blog",
+        });
+});
+
 module.exports = router;
